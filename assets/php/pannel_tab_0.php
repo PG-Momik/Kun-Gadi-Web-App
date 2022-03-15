01e6028f89b4b3df6d4ea062f2f28a68bf1f583f
PG-Momik <momik12339@gmail.com>
2022-03-16 00:00:00 +0545

    Finished pannel_tab_0.php

---
+++ assets/php/pannel_tab_0.php
@@ -0,0 +1,16 @@
+<?php include_once 'partials/_html_p1.php'; ?>
+<title>Dashboard</title>
+<?php include_once 'partials/_html_p2.php'; ?>
+<?php include_once 'partials/_sessionStart.php'; ?>
+
+<body>
+  <?php include_once 'partials/_nav.php'; ?>
+  <div id="admin-content">
+    <?php include_once 'partials/_sidebar.php'; ?>
+    <div class="admin-right">
+      <h1>Welcome Momik</h1>
+    </div>
+  </div>
+</body>
+
+</html>
