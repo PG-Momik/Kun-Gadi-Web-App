5a60dad19b8c8d8d79b5e93323b22a73d5dab0fa
PG-Momik <momik12339@gmail.com>
2022-04-04 00:00:00 +0545

    Finished _sidebar.php

---
+++ assets/php/partials/_sidebar.php
@@ -0,0 +1,39 @@
+<div id="sidenav" class="admin-left" style="color:white;">
+    <ul id="side-nav-menu" style="list-style: none;">
+        <li class="nav-item">
+            <a href="pannel_tab_0.php" class="nav-link">
+                Dashboard <i class="fas fa-users-cog"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pannel_tab_1.php" class="nav-link">
+                Users <i class="fas fa-users-cog"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pannel_tab_2.php" class="nav-link">
+                Nodes <i class="fas fa-chart-bar"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pannel_tab_3.php" class="nav-link">
+                Routes <i class="fas fa-dolly-flatbed"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pannel_tab_4.php" class="nav-link">
+                Node Requests<i class="fas fa-money-bill-wave"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pannel_tab_5.php" class="nav-link">
+                Route Requests<i class="fas fa-dolly"></i>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a href="pannel_tab_6.php" class="nav-link">
+                Logout <i class="fas fa-sign-in-alt"></i>
+            </a>
+        </li>
+    </ul>
+</div>
